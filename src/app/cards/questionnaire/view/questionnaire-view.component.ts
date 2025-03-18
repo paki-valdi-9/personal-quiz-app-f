24a610a9dad0ec86a875f1416af1fa0b9aa0c046
paki-valdi-9 <palkourbanek45@gmail.com>
2025-03-18 21:37:49 +0100

    refactor: unused import

--- src/app/cards/questionnaire/view/questionnaire-view.component.ts
+++ src/app/cards/questionnaire/view/questionnaire-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
