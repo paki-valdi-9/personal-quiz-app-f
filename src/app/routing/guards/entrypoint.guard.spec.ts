3327cc5bdb59e05e2fcd4f0271ac72d04d46dece
paki-valdi-9 <palkourbanek45@gmail.com>
2025-03-19 22:57:51 +0100

    fix: entry guard tests

--- src/app/routing/guards/entrypoint.guard.spec.ts
+++ src/app/routing/guards/entrypoint.guard.spec.ts
@@ -3,9 +3,10 @@ import { entryPointGuard } from './entrypoint.guard';
 import { Router } from '@angular/router';
 import { TestBed } from '@angular/core/testing';
 import { Observable } from 'rxjs';
+import { MockService } from 'ng-mocks';
 
 describe('EntrypointGuard', () => {
-  let mockRouter: Partial<Router>;
+  let mockRouter: Router;
   let mockRoute: ActivatedRouteSnapshot;
   let mockState: RouterStateSnapshot;
 
@@ -13,22 +14,23 @@ describe('EntrypointGuard', () => {
 
   //Mock Dependencies
   beforeEach(() => {
-    mockRouter = {
-      navigate: jest.fn(),
-    };
+    mockRouter = MockService(Router);
     mockRoute = {} as ActivatedRouteSnapshot;
     mockState = {
       url: '/',
     } as RouterStateSnapshot;
   });
+
   it('should navigate to meet-me if path ends with /', () => {
+    const navigateSpy = jest.spyOn(mockRouter, 'navigate');
+
     TestBed.runInInjectionContext(() => {
       result$ = entryPointGuard(mockRoute, mockState) as Observable<boolean>;
     });
 
     result$.subscribe((result) => {
-      expect(result).toBeFalse();
-      expect(mockRouter.navigate).toHaveBeenCalledWith(['/meet-me'], {
+      expect(result).toBeFalsy();
+      expect(navigateSpy).toHaveBeenCalledWith(['/meet-me'], {
         replaceUrl: true,
         onSameUrlNavigation: 'reload',
       });
@@ -36,13 +38,53 @@ describe('EntrypointGuard', () => {
   });
 
   it('should navigate to meet-me if path does not ends with meet-me', () => {
+    const navigateSpy = jest.spyOn(mockRouter, 'navigate');
+
     TestBed.runInInjectionContext(() => {
       result$ = entryPointGuard(mockRoute, mockState) as Observable<boolean>;
     });
 
     result$.subscribe((result) => {
-      expect(result).toBeFalse();
-      expect(mockRouter.navigate).toHaveBeenCalledWith(['/meet-me'], {
+      expect(result).toBeFalsy();
+      expect(navigateSpy).toHaveBeenCalledWith(['/meet-me'], {
+        replaceUrl: true,
+        onSameUrlNavigation: 'reload',
+      });
+    });
+  });
+
+  it('should navigate to quiz if path includes quiz', () => {
+    const navigateSpy = jest.spyOn(mockRouter, 'navigate');
+
+    mockState = {
+      url: '/quiz',
+    } as RouterStateSnapshot;
+    TestBed.runInInjectionContext(() => {
+      result$ = entryPointGuard(mockRoute, mockState) as Observable<boolean>;
+    });
+
+    result$.subscribe((result) => {
+      expect(result).toBeFalsy();
+      expect(navigateSpy).toHaveBeenCalledWith(['/quiz'], {
+        replaceUrl: true,
+        onSameUrlNavigation: 'reload',
+      });
+    });
+  });
+
+  it('should navigate to quiz if path includes quiz and does not ends with quiz', () => {
+    const navigateSpy = jest.spyOn(mockRouter, 'navigate');
+
+    mockState = {
+      url: '/quiz/a',
+    } as RouterStateSnapshot;
+    TestBed.runInInjectionContext(() => {
+      result$ = entryPointGuard(mockRoute, mockState) as Observable<boolean>;
+    });
+
+    result$.subscribe((result) => {
+      expect(result).toBeFalsy();
+      expect(navigateSpy).toHaveBeenCalledWith(['/quiz'], {
         replaceUrl: true,
         onSameUrlNavigation: 'reload',
       });
